b27d2539346bf88f22ba67747c1c002dd538af42
Alejandro Luces González <aluces@polytex.local>
2023-08-03 17:05:32 -0400

    Add_algoritmo_method_string_27

---
+++ algoritmo_method_string_27.js
@@ -0,0 +1,15 @@
+// String.prototype.startsWith()
+// El startsWith()método determina si una cadena comienza con los caracteres de una cadena especificada, regresando trueo falsesegún corresponda.
+
+// Algorirmo de busqueda:
+
+let str="Hola mundo";
+
+const strSearch =(a) =>{
+
+    return str.startsWith(a);
+}
+console.log(strSearch("Hola"));
+
+    
+
