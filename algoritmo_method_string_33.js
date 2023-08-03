153d344baa04bf3d89716e15956f7f8ec6e12156
Alejandro Luces González <aluces@polytex.local>
2023-08-03 18:09:40 -0400

    Add_algoritmo_method_string_33

---
+++ algoritmo_method_string_33.js
@@ -0,0 +1,10 @@
+// Cadena.prototipo.toUpperCase()
+// El toUpperCase()método devuelve el valor de la cadena de llamada convertido a mayúsculas (el valor se convertirá en una cadena si no lo es).
+
+// Algoritmo de ejemplo:
+
+let cadena = "Hola Mundo";
+const strMay =(a) =>{
+return a.toUpperCase();
+}
+console.log(strMay(cadena));
