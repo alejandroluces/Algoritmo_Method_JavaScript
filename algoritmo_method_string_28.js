3cfe1cd8ecd920ad50a6a8d4e29940142d107b19
Alejandro Luces González <aluces@polytex.local>
2023-08-03 17:08:44 -0400

    Add_algoritmo_method_string_28

---
+++ algoritmo_method_string_28.js
@@ -0,0 +1,12 @@
+// Cadena.prototipo.subcadena()
+// El substring()método devuelve la parte del stringíndice desde el inicio hasta el índice final y excluyendolo, o hasta el final de la cadena si no se proporciona un índice final.
+
+// Algoritmo de ejemplo:
+
+let cadena = 'Mozilla';
+
+const strSubs =(a) =>{
+return a.substring(2, 3);
+}
+
+console.log(strSubs(cadena));
