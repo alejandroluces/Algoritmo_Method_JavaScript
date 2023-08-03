8af0c3bcee8f9749d8dd7064e872d039f70b557f
Alejandro Luces González <aluces@polytex.local>
2023-08-03 16:04:22 -0400

    Add_algoritmo_method_string_22

---
+++ algoritmo_method_string_22.js
@@ -0,0 +1,16 @@
+// Cadena.prototipo.reemplazar()
+// El replace()método devuelve una nueva cadena con una, algunas o todas las coincidencias de un patternreemplazadas por un replacement. puede patternser una cadena o un RegExp, y replacementpuede ser una cadena o una función llamada para cada coincidencia. Si patternes una cadena, solo se reemplazará la primera aparición. La cadena original se deja sin cambios.
+
+// // Algoritmo de ejemplo:
+//
+let str= "Tengo un perro y un gato";
+let newStr = str.replace("perro", "gato");
+console.log(newStr);
+
+// Si usamos una funcion seria asi:
+
+let str2= "Tengo una hija que se llama Sara";
+const replaceStr =(a) =>{
+return a.replace("Sara", "gato");
+}
+console.log(replaceStr(str2));
