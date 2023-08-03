68808ac09197bffe698f2d51fdcfc97328502cf7
Alejandro Luces González <aluces@polytex.local>
2023-08-03 17:17:55 -0400

    Add_algoritmo_method_string_29

---
+++ algoritmo_method_string_29.js
@@ -0,0 +1,10 @@
+// Cadena.prototipo.toLocaleLowerCase()
+// El toLocaleLowerCase()método devuelve el valor de la cadena de llamada convertido a minúsculas, de acuerdo con las asignaciones de casos específicas de la configuración regional.
+
+// Algoritmo de ejemplo:
+
+let cadena = 'MoziLLa';
+const wordMin =(a) =>{
+return a.toLocaleLowerCase();
+}
+console.log(wordMin(cadena));
