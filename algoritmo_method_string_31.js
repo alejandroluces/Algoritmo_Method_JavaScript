20989eb3a74be88337085b26085517eecc58f81f
Alejandro Luces González <aluces@polytex.local>
2023-08-03 17:25:50 -0400

    Add_algoritmo_method_string_31

---
+++ algoritmo_method_string_31.js
@@ -0,0 +1,11 @@
+// Cadena.prototipo.toLowerCase()
+// El toLowerCase()método devuelve el valor de la cadena de llamada convertido a minúsculas.
+
+// Algoritmo de ejemplo:
+
+let cadena = "HHola Mundo";
+const str =(a) =>{
+return a.toLowerCase();
+}
+console.log(str(cadena));
+
