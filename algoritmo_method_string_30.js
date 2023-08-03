7981199a264412104a2d34b541e44725ecf51e5f
Alejandro Luces González <aluces@polytex.local>
2023-08-03 17:19:47 -0400

    Add_algoritmo_method_string_30

---
+++ algoritmo_method_string_30.js
@@ -0,0 +1,11 @@
+// Cadena.prototipo.toLocaleUpperCase()
+// El toLocaleUpperCase()método devuelve el valor de la cadena de llamada convertido a mayúsculas, de acuerdo con las asignaciones de casos específicas de la configuración regional.
+
+// Algoritmo de ejemplo:
+
+let cadena = 'MoziLLa';
+const wordMay =(a) =>{
+return a.toLocaleUpperCase();
+}
+console.log(wordMay(cadena));
+
