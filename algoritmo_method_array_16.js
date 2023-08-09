5f54a603e5c3f44cd14c0d6e72d289f761857d23
Alejandro Luces González <aluces@polytex.local>
2023-08-09 18:05:11 -0400

    Add_algoritmo_method_array_16

---
+++ algoritmo_method_array_16.js
@@ -0,0 +1,15 @@
+// Array.prototype.includes()
+// The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+
+// Algoritmo de ejemplo:
+
+let array = [1, 2, 3, 4, 5];
+
+const includes = (array) => {
+    return array.includes(3);
+    }
+
+console.log(includes(array)); // true
+
+
+
