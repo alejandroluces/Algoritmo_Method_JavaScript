eb621d783cd9b4376449bc841e9412cc34902dec
Alejandro Luces González <aluces@polytex.local>
2023-08-09 18:05:42 -0400

    Add_algoritmo_method_array_19

---
+++ algoritmo_method_array_19.js
@@ -0,0 +1,13 @@
+// Array.prototype.join()
+// El método join() une todos los elementos de una matriz (o un objeto similar a una matriz) en una cadena y devuelve esta cadena.
+
+// Algoritmo de ejemplo:
+
+let array = [1, 2, 3, 4, 5];
+
+const joinArray = (array) => {
+    return array.join("");
+    }   
+
+console.log(joinArray(array)); // 1,2,3,4,5
+
