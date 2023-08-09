33ed84af73c7eedb07f8244bbf0ff4f01f871dd7
Alejandro Luces González <aluces@polytex.local>
2023-08-09 18:05:32 -0400

    Add_algoritmo_method_array_18

---
+++ algoritmo_method_array_18.js
@@ -0,0 +1,13 @@
+// Array.isArray()
+
+// El Array.isArray()método estático determina si el valor pasado es un archivo Array.
+
+// Algoritmo de ejemplo:
+
+let array = [1, 2, 3, 4, 5];
+
+const isArray = (array) => {
+    return Array.isArray(array);
+    }
+
+console.log(isArray(array)); // true
