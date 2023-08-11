0f3af0f06f73c5d53b9a1944add3a6414abd34cd
Alejandro Luces González <aluces@polytex.local>
2023-08-10 21:17:34 -0400

    Add_algoritmo_method_array_36

---
+++ algoritmo_method_array_36.js
@@ -0,0 +1,12 @@
+// Array.prototype.toString()
+// El toString()método de Arrayinstancias devuelve una cadena que representa la matriz especificada y sus elementos.
+
+// Algoritmo de ejemplo:
+
+const array1 = [1, 2, 'a', '1a'];
+
+const array2 = (array) => {
+  return array.toString();
+}
+
+console.log(array2(array1));
